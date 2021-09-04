3d04629739dc71ee5e9d0ddbc7365ac4ec41d9e7
prochell <prochell@student.21-school.ru>
2021-09-04 19:39:11 +0300

    fill to norm env.c

--- src/env.c
+++ src/env.c
@@ -6,7 +6,7 @@
 /*   By: prochell <prochell@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/04 00:04:48 by prochell          #+#    #+#             */
-/*   Updated: 2021/09/04 19:36:03 by prochell         ###   ########.fr       */
+/*   Updated: 2021/09/04 19:38:53 by prochell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,13 +15,14 @@
 void	get_envp(char **env, t_envp **environment)
 {
 	char	**tmp;
+	int		i;
 
-	int i;
 	i = 0;
 	while (env[i])
 	{
 		tmp = ft_split(env[i], '=');
-		ft_lstadd_back_minishell(environment, ft_lstnew_minishell(tmp[0], tmp[1]));
+		ft_lstadd_back_minishell(environment, \
+			ft_lstnew_minishell(tmp[0], tmp[1]));
 		i++;
 	}
 }
