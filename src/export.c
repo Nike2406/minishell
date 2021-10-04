7d1d6985197950bac8d04f5e25cb12745432a851
prochell <prochell@student.21-school.ru>
2021-10-04 19:43:24 +0300

    fullfill export sorting

--- src/export.c
+++ src/export.c
@@ -6,7 +6,7 @@
 /*   By: prochell <prochell@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/15 16:32:48 by prochell          #+#    #+#             */
-/*   Updated: 2021/10/03 21:18:57 by prochell         ###   ########.fr       */
+/*   Updated: 2021/10/04 19:40:42 by prochell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,13 +19,11 @@ void	ft_lst_swap(t_envp **swap)
 	t_envp	*second;
 	t_envp	*third;
 
+	// (((*swap)->next)->next) = NULL;
 	second = NULL;
-	if (!(*swap)->next->next)
+	if (!(*swap)->prev)
 	{
-		*swap = ft_lstfirst_minishell(*swap);
-	}
-	else if (!(*swap)->prev)
-	{
+		// (*cont)++;
 		if ((*swap) == NULL || (*swap)->next == NULL)
 			return ;
 		first = *swap;
@@ -40,6 +38,7 @@ void	ft_lst_swap(t_envp **swap)
 	}
 	else
 	{
+		// (*cont)++;
 		zero = (*swap)->prev;
 		first = *swap;
 		first->prev = (*swap)->next;
@@ -56,28 +55,84 @@ void	ft_lst_swap(t_envp **swap)
 
 int	sort_export(t_envp *tmp_env)
 {
-	int		cont;
+	// int count;
+	int i;
+	int j;
+	int list_size;
 
-	cont = 0;
-	while (tmp_env->next->next)
+	// count = -1;
+	list_size = 0;
+	while (tmp_env->next)
 	{
-		if (!tmp_env->next->key)
-			break;
-		if (ft_strcmp(tmp_env->key, tmp_env->next->key) > 0)
-		{
-			ft_lst_swap(&tmp_env);
-			cont++;
-		}
 		tmp_env = tmp_env->next;
+		list_size++;
 	}
-	if (!cont)
+
+	i = 0;
+	while (i < list_size - 1)
 	{
+		j = 0;
 		tmp_env = ft_lstfirst_minishell(tmp_env);
-		check_list(tmp_env);
-		return (0);
+		while (j < list_size - i - 1)
+		{
+			if (ft_strcmp(tmp_env->key, tmp_env->next->key) > 0)
+			{
+				ft_lst_swap(&tmp_env);
+			}
+			tmp_env = tmp_env->next;
+			j++;
+		}
+		i++;
 	}
-	sort_export(tmp_env);
+	check_list(tmp_env);
+
+	// while (tmp_env->next)
+	// {
+	// 	count = 0;
+	// 	tmp_env = head;
+	// 	if (ft_strcmp(tmp_env->key, tmp_env->next->key) > 0)
+	// 	{
+	// 		// if (!tmp_env->next->next)
+	// 		// {
+	// 		// 	tmp_env = head;
+	// 		// 	count = 0;
+	// 		// 	continue;
+	// 		// }
+	// 		ft_lst_swap(&tmp_env, &count);
+	// 	}
+	// 	if (!count)
+	// 	{
+	// 		check_list(tmp_env);
+	// 		break;
+	// 	}
+	// 	tmp_env = tmp_env->next;
+	// }
+
 	return (1);
+	// int	cont;
+
+	// cont = 0;
+	// while (tmp_env->next)
+	// {
+	// 	if (!tmp_env->next->key)
+	// 		break;
+	// 	if (ft_strcmp(tmp_env->key, tmp_env->next->key) > 0)
+	// 	{
+	// 		ft_lst_swap(&tmp_env, &cont);
+	// 	}
+	// 	if (!cont)
+	// 		break;
+	// 	tmp_env = tmp_env->next;
+	// }
+	// if (!cont)
+	// {
+	// 	tmp_env = ft_lstfirst_minishell(tmp_env);
+		// check_list(tmp_env);
+	// 	return (0);
+	// }
+	// sort_export(tmp_env);
+	// return (1);
+
 }
 
 int		get_export(t_shell *minishell, char **str)
